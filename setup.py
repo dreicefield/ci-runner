8ee581df40f1874638494809a45a6597d9a1762a
Eisfeld Andrej <andrej.eisfeld2@de.bosch.com>
2022-02-27 18:13:20 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="ci-runner",
     version="0.1.0",
     description="General pipeline execution engine",
     python_requires=">3.6.0",
-    # install_requires=requirements,
+    packages=find_packages(where="src"),
+    package_dir={'': "src"},
     include_package_data=True,
+    install_requires=[
+        "click==7.1.2",
+        "networkx==2.5.1",
+        #"pyyaml==5.2",
+    ],
     entry_points={
         "console_scripts": ["ci=ci.cli:main"],
     },
+    url='https://github.com/dreicefield/ci-runner',
 )
